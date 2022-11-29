e606eeefee0141539887d98ba58c3cd924cd991e
ValeriyDolgov <dolgov.valerka@mail.ru>
2022-11-29 14:10:39 +0600

    Add migration for DB of project, next is creating basic Spring MVC

---
+++ src/main/java/org/qazdevelop/qazdevinfo/QazdevinfoApplication.java
@@ -0,0 +1,13 @@
+package org.qazdevelop.qazdevinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QazdevinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QazdevinfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/qazdevelop/qazdevinfo/QazdevinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qazdevelop.qazdevinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QazdevinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
